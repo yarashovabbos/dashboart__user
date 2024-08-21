18f7666e63e023700bd41d8839667961790b5e8d
yarashov abbos <abbosdev98@gmail.com>
2024-08-21 23:25:50 +0500

    jhon doe

---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+// src/types/User.ts
+
+export interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    // Add any other properties that a user might have
+  }
+  
